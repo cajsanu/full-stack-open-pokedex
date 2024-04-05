1d90b031813e06d57050d5a295835cc7c94d215e
Cajsa Nummelin <cajsanummelin@Aleksandrs-Air.home>
2024-04-05 13:21:59 +0300

    Added simple e2e test using cypress

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from "express";
+const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(express);
+app.use(express.static("dist"));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
